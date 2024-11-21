6e6edca51561917411c72bc90591e7209890c1e4
Jack Woo <bandit@MacBook.local>
2024-11-20 19:53:46 -0800

    Cleaning up code

--- reader.py
+++ reader.py
@@ -1,24 +1,6 @@
 import serial
 import serial.tools.list_ports
 
-ports = list(serial.tools.list_ports.comports())
-ourport = [port for port in ports if "usbserial" in port.device]
-if len(ourport) == 0:
-    print("No serial device found")
-    exit(1)
-ourport = ourport[0].device
-
-ser = serial.Serial(
-    port=ourport,\
-    baudrate=9600,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-        timeout=0)
-
-print("connected to: " + ser.portstr)
-# count=1
-
 def handle_state_change(ser):
     state = bool.from_bytes(ser.read(1))
     if state:
@@ -31,21 +13,50 @@ def card_read(ser):
     data = ser.read(num_bytes)
     print(data)
 
-commands = {
+COMMANDS = {
     0x01: handle_state_change,
     0x02: card_read
 }
 
-while True:
-    if ser.in_waiting == 0:
-        continue
-    command, = ser.read(1)
-    if command not in commands:
-        print(f"Unknown command: {command}")
-        # Flush the buffer
-        while ser.in_waiting > 0:
-            ser.read(1)
+def find_port():
+    arduino_port_search_list = ["Arduino", "CH340", "usbserial"]
 
-    commands[command](ser)
+    ports = list(serial.tools.list_ports.comports())
+    ourport = [port for port in ports if any(arduino_port in port.device for arduino_port in arduino_port_search_list)]
+    if len(ourport) == 0:
+        return None
+    ourport = ourport[0].device
+    return ourport
 
-# ser.close()
+def reader_loop(ser):
+    try:
+        while True:
+            if ser.in_waiting == 0:
+                continue
+            command, = ser.read(1)
+            if command not in COMMANDS:
+                print(f"Unknown command: {command}")
+                # Flush the buffer
+                while ser.in_waiting > 0:
+                    ser.read(1)
+
+            COMMANDS[command](ser)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        ser.close()
+
+def main():
+    arduino_port = find_port()
+
+    ser = serial.Serial(
+        port=arduino_port,
+        baudrate=9600
+    )
+
+    print("Connected To: " + ser.portstr)
+    reader_loop(ser)
+
+
+if __name__ == "__main__":
+    main()
