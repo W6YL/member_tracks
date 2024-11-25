51e2233826e1db21bb3dd8e920ff92a4cdf3c93b
Bandit <woo.jack.06@gmail.com>
2024-11-25 14:35:10 -0800

    Pretty formatting

--- controller_bot/bot.py
+++ controller_bot/bot.py
@@ -34,7 +34,7 @@ def generate_members_embed(members):
         chunk = members[i:i+2]
         for member in chunk:
             last_login = member["last_timestamp"].strftime("%s")
-            embed.add_field(name=f"{member['first_name']} {member['last_name'][:1]}.", value=f"{member['position_in_club']}\nLogged in <t:{last_login}:R>", inline=True)
+            embed.add_field(name=f"{member['first_name']} {member['last_name'][:1]}.", value=f"{member['position_in_club'].title()}\nLogged in <t:{last_login}:R>", inline=True)
         embed.add_field(name="\u200b", value="\u200b")
     return embed
 
